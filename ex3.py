88730c4eb1f0f44bac1809634224a626fbe24655
Zander Galluppi <agalluppi@uchicago.edu>
2017-10-12 15:30:06 -0500

    Finished Python assignments

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# Prints the text
+print("I will now count my chickens:")
+
+# Prints "Hens 30.0"
+print("Hens", 25.0 + 30.0 / 6.0)
+# Prints "Roosters 97"
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+# Prints the text
+print("Now I will count the eggs:")
+
+# Prints "6.75"
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Prints the text
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints whether the given inequality is true or false (it's false)
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Prints "What is 3+2?" and then the answer
+print("What is 3 + 2?", 3.0 + 2.0)
+# Prints "What is 5-7?" and then the answer
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# Prints the text
+print("Oh, that's why it's False.")
+
+# Prints the text
+print("How about some more.")
+
+# Prints whether the given inequality is true or false (it's true)
+print("Is it greater?", 5.0 > -2.0)
+# Prints whether the given inequality is true or false (it's true)
+print("Is it greater or equal?", 5.0 >= -2.0)
+# Prints whether the given inequality is true or false (it's false)
+print("Is it less or equal?", 5.0 <= -2.0)
---
+++ ex3_1.py
@@ -0,0 +1,5 @@
+# Calculate quarterly student package for BSD
+print("Tuition = $17584")
+print("Health insurance = $1324")
+print("Student life = $305")
+print("Total = $",17584 + 1324 + 305)
